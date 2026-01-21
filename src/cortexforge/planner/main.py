99c6c58bb24f56520eea8f7f502cb82438997a0a
Andreaj42 <andrea.joly1989@gmail.com>
2026-01-21 22:24:07 +0100

    :bug: remove useless code

--- src/cortexforge/planner/main.py
+++ src/cortexforge/planner/main.py
@@ -1,5 +1,3 @@
-import os
-
 from cortexforge.cli.planner import parse_args
 from cortexforge.planner.generators.cortex_scenario import generate_cortex_scenario
 from cortexforge.planner.generators.experiment_scenario import ExperimentScenario
@@ -12,8 +10,6 @@ logger = setup_logger()
 def main():
     logger.info("Starting scenario generation...")
     args = parse_args()
-
-    os.makedirs("scenarios", exist_ok=True)
 
     try:
         nodes = load_nodes(args.nodes_path)
@@ -30,11 +26,11 @@ def main():
         image="ghcr.io/andreaj42/cortexforge:latest",
         command='bash -lc "python3 /cortexlab/homes/andrea_joly/CorteXForge/src/cortexforge/forge/main.py"',
         description="Dataset Generator",
-        output_path="scenarios/scenario.yaml",
+        output_path="configs/scenario.yaml",
     )
 
     experiment = ExperimentScenario(nodes=nodes, duration=args.duration)
-    experiment.to_csv("scenarios/timeline.csv")
+    experiment.to_csv("configs/timeline.csv")
 
     logger.info("Scenario generation completed.")
 
