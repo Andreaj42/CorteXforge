3ab8f286eade7a49196efd1bd0e6d507a85dda24
Andreaj42 <andrea.joly1989@gmail.com>
2026-01-23 10:33:23 +0100

    :memo: sonarqube compliant

--- src/cortexforge/forge/utils/node_identity.py
+++ src/cortexforge/forge/utils/node_identity.py
@@ -1,9 +1,11 @@
-import socket
+"""Get node name identity."""
+
+from socket import gethostname
 
 
 def get_node_name(logger=None) -> str:
     try:
-        hostname = socket.gethostname()
+        hostname = gethostname()
         return hostname
     except Exception as e:
         logger.error(f"Error getting hostname: {e}")
