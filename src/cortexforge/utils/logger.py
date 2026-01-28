873bd020e889f338af4c5be224be6f7de2112a3b
Andreaj42 <andrea.joly1989@gmail.com>
2026-01-28 01:07:23 +0100

    :art: improve logger format

--- src/cortexforge/utils/logger.py
+++ src/cortexforge/utils/logger.py
@@ -12,7 +12,8 @@ def setup_logger(level: int = logging.INFO) -> logging.Logger:
         console_handler = logging.StreamHandler()
         console_handler.setLevel(level)
         formatter = logging.Formatter(
-            "[%(asctime)s] [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+            "[%(asctime)s] [%(levelname)s][%(filename)s:] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
         )
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
