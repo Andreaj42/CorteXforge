c70777f95c5dc32abf900530555a7421e6ba7bd6
Andréa Joly <andrea@andrea-joly.fr>
2026-01-29 17:50:56 +0100

    :sparkles: force datatype (sc16)

--- src/cortexforge/forge/radio/rx_recorder.py
+++ src/cortexforge/forge/radio/rx_recorder.py
@@ -13,7 +13,7 @@ class RxRecorder(gr.top_block):
 
         self.src = uhd.usrp_source(
             usrp_args,
-            uhd.stream_args(cpu_format="fc32", channels=[0]),
+            uhd.stream_args(cpu_format="sc16", channels=[0]),
         )
         self.src.set_clock_source("internal", 0)
         self.src.set_samp_rate(rate)
@@ -21,7 +21,7 @@ class RxRecorder(gr.top_block):
         self.src.set_gain(gain, 0)
         self.src.set_antenna("TX/RX", 0)
 
-        self.sink = blocks.file_sink(gr.sizeof_gr_complex, out_path, False)
+        self.sink = blocks.file_sink(gr.sizeof_short * 2, out_path, False)
         self.sink.set_unbuffered(False)
 
         self.connect(self.src, self.sink)
