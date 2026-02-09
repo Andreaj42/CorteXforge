83c91a0919fb69c70ea76e4cdfa9f3923263d427
Andréa Joly <andrea@andrea-joly.fr>
2026-02-09 16:29:21 +0100

    :art: refactor TxBurst to use tx_channel variable for channel configuration

--- src/cortexforge/forge/radio/tx_burst.py
+++ src/cortexforge/forge/radio/tx_burst.py
@@ -15,15 +15,17 @@ class TxBurst(gr.top_block):
         iq_np = np.asarray(iq, dtype=np.complex64)
         iq_list = iq_np.tolist()
 
+        self.tx_channel = 0
+
         self.src = blocks.vector_source_c(iq_list, repeat=False)
         self.sink = uhd.usrp_sink(
             usrp_args,
-            uhd.stream_args(cpu_format="fc32", channels=[0]),
+            uhd.stream_args(cpu_format="fc32", channels=[self.tx_channel]),
         )
-        self.sink.set_clock_source("internal", 0)
+        self.sink.set_clock_source("internal", self.tx_channel)
         self.sink.set_samp_rate(rate)
-        self.sink.set_center_freq(freq, 0)
-        self.sink.set_gain(gain, 0)
-        self.sink.set_antenna("TX/RX", 0)
+        self.sink.set_center_freq(freq, self.tx_channel)
+        self.sink.set_gain(gain, self.tx_channel)
+        self.sink.set_antenna("TX/RX", self.tx_channel)
 
         self.connect(self.src, self.sink)
